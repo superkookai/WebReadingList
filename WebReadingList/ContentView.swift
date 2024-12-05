f5c21e16250c8eebcd3ebdd93b0ddf72bcd77fad
Weerawut <weerawut@live.com>
2024-12-05 09:00:11 +0700

    Initial Commit

---
+++ WebReadingList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebReadingList
+//
+//  Created by Weerawut Chaiyasomboon on 5/12/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebReadingList/WebReadingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebReadingListApp.swift
+//  WebReadingList
+//
+//  Created by Weerawut Chaiyasomboon on 5/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct WebReadingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
